c358f8f76bda191f5b1c013d53f558bf55821b2d
alelagana <46794432+alelagana@users.noreply.github.com>
2026-04-25 19:26:13 +0000

    Deploying to gh-pages from @ Lagana-Lab/lagana-lab.github.io@d3a2059b29bbe8691cc6fa3901210c1f185e9f58 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -419,9 +419,9 @@ ninja.data = [{
           description: "Defining drivers of relapse and resistance to improve outcomes in high-risk myeloma",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
+            },},{id: "projects-drivers-of-progression-resistance-amp-high-risk-disease",
+          title: 'Drivers of Progression, Resistance &amp;amp; High-Risk Disease',
+          description: "Defining drivers of relapse and resistance to improve outcomes in high-risk myeloma",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
             },},{id: "projects-project-4",
