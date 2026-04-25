d0af914efa093304b0cb1e27a5d5516fd36d7274
alelagana <46794432+alelagana@users.noreply.github.com>
2026-04-25 19:13:57 +0000

    Deploying to gh-pages from @ Lagana-Lab/lagana-lab.github.io@73fe9be0db16a31c866b982c717c61ab865303a4 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -414,9 +414,9 @@ ninja.data = [{
           description: "Modeling high-throughput data to generate novel insights into myeloma biology",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+            },},{id: "projects-drivers-of-progression-resistance-amp-high-risk-disease",
+          title: 'Drivers of Progression, Resistance &amp;amp; High-Risk Disease',
+          description: "Modeling high-throughput data to generate novel insights into myeloma biology",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
             },},{id: "projects-project-3-with-very-long-name",
